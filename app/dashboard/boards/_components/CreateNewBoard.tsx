57ff072ccc017abae7318faef9acb8afeceab8e3
yusufuysal <yusufuysal.dev@gmail.com>
2024-11-22 01:11:37 +0300

    Close the dialog after creating a new board

--- app/dashboard/boards/_components/CreateNewBoard.tsx
+++ app/dashboard/boards/_components/CreateNewBoard.tsx
@@ -16,6 +16,7 @@ import { Label } from "@/components/ui/label";
 import { cn } from "@/utils/cn";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
@@ -27,6 +28,8 @@ const newBoardSchema = z.object({
 type TNewBoardSchema = z.infer<typeof newBoardSchema>;
 
 export const CreateNewBoard = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -46,6 +49,7 @@ export const CreateNewBoard = () => {
     if (result?.success) {
       toast.success("Board created successfully!");
       reset();
+      setIsOpen(false);
       router.push(`/dashboard/boards/${result.slug}`); // Navigate to the new board
     } else {
       toast.error(result?.error || "An unexpected error occurred.");
@@ -53,7 +57,7 @@ export const CreateNewBoard = () => {
   };
 
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger className="sidebar-button h-[48px] w-[240px] justify-start gap-4 rounded-r-full px-0 py-0 pl-10 text-heading-m font-[500] text-primary md:gap-[13px] lg:w-[276px] lg:gap-[17px]">
         {" "}
         <BoardIcon alt="Board Icon" width="16" height="16" />+ Create New Board
