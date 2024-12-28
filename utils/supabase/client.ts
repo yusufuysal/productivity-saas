a2bd7a5adfb7cdb077a6edb91fdb4aed1cf464b0
yusufuysal <yusufuysal.dev@gmail.com>
2024-12-29 02:30:19 +0300

    Use new clients for supabase

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,19 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
 
-export const createClient = () =>
-  createBrowserClient(
+const supabaseClient = async (supabaseAccessToken: string) => {
+  const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      global: {
+        headers: {
+          Authorization: `Bearer ${supabaseAccessToken}`,
+        },
+      },
+    },
   );
+
+  return supabase;
+};
+
+export default supabaseClient;
